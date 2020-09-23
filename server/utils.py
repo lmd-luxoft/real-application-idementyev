1d7bf6331345b560f570a41433b042bafd055e0d
idementyev <idementyev@luxoft.com>
2020-09-24 01:01:46 +0300

    Change timestamps from local time to proper UTC time.

--- server/utils.py
+++ server/utils.py
@@ -5,7 +5,8 @@ __date__ = '2020-09-23'
 
 import random
 import string
-from datetime import datetime
+from datetime import datetime, timezone
+
 
 filename_len = 8
 
@@ -27,7 +28,7 @@ def generate_string() -> str:
     _letters = string.ascii_letters
     _digits = string.digits
     _chars = _letters + _digits
-    _str = ''.join(random.choice(_chars) for i in range(filename_len))
+    _str = ''.join(random.choice(_chars) for _ in range(filename_len))
     return _str
 
 
@@ -35,11 +36,12 @@ def convert_date(timestamp: float) -> str:
     """Convert date from timestamp to string.
 
     Example of date format: 2019-09-05 11:22:33.
+    Date is considered to be in UTC as atime, mtime, ctime are stored in UTC.
 
     Args:
         timestamp (float): date timestamp.
     Returns:
         str: converted date.
     """
-    _out_format = '%Y-%m-%d %H:%M:%S'
-    return datetime.fromtimestamp(timestamp).strftime(_out_format)
+    _format = '%Y-%m-%d %H:%M:%S'
+    return datetime.fromtimestamp(timestamp, timezone.utc).strftime(_format)
