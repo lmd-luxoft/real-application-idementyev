42c14d3032c7d373d363317546f96cf976c0ebc4
idementyev <idementyev@luxoft.com>
2020-09-28 13:20:51 +0300

    Ground work.

--- server/handler.py
+++ server/handler.py
@@ -1,5 +1,7 @@
-# Copyright 2019 by Kirill Kanin.
-# All rights reserved.
+__version__ = '0.5.0'
+__author__ = 'idementyev@luxoft.com'
+__date__ = '2020-09-28'
+
 
 import json
 from aiohttp import web
@@ -26,94 +28,88 @@ class Handler:
 
         Args:
             request (Request): aiohttp request.
-
         Returns:
             Response: JSON response with status.
-
         """
-
         pass
 
-    @UsersAPI.authorized
-    @RoleModel.role_model
+    # @UsersAPI.authorized
+    # @RoleModel.role_model
     # @UsersSQLAPI.authorized
     # @RoleModelSQL.role_model
-    async def get_files(self, request: web.Request, *args, **kwargs) -> web.Response:
+    async def get_files(self, request: web.Request,
+                        *args, **kwargs) -> web.Response:
         """Coroutine for getting info about all files in working directory.
 
         Args:
             request (Request): aiohttp request.
-
         Returns:
-            Response: JSON response with success status and data or error status and error message.
-
+            Response: JSON response with success status and data or error
+            status and error message.
         """
-
         pass
 
-    @UsersAPI.authorized
-    @RoleModel.role_model
+    # @UsersAPI.authorized
+    # @RoleModel.role_model
     # @UsersSQLAPI.authorized
     # @RoleModelSQL.role_model
-    async def get_file_info(self, request: web.Request, *args, **kwargs) -> web.Response:
+    async def get_file_info(self, request: web.Request,
+                            *args, **kwargs) -> web.Response:
         """Coroutine for getting full info about file in working directory.
 
         Args:
-            request (Request): aiohttp request, contains filename and is_signed parameters.
-
+            request (Request): aiohttp request, contains filename and
+            is_signed parameters.
         Returns:
-            Response: JSON response with success status and data or error status and error message.
-
+            Response: JSON response with success status and data or error
+            status and error message.
         Raises:
             HTTPBadRequest: 400 HTTP error, if error.
-
         """
-
         pass
 
-    @UsersAPI.authorized
-    @RoleModel.role_model
+    # @UsersAPI.authorized
+    # @RoleModel.role_model
     # @UsersSQLAPI.authorized
     # @RoleModelSQL.role_model
-    async def create_file(self, request: web.Request, *args, **kwargs) -> web.Response:
+    async def create_file(self, request: web.Request,
+                          *args, **kwargs) -> web.Response:
         """Coroutine for creating file.
 
         Args:
-            request (Request): aiohttp request, contains JSON in body. JSON format:
+            request (Request): aiohttp request, contains JSON in body.
+            JSON format:
             {
                 "content": "string. Content string. Optional",
-                "security_level": "string. Security level. Optional. Default: low",
-                "is_signed": "boolean. Sign or not created file. Optional. Default: false"
-            }.
-
+                "security_level": "string. Security level. Optional.
+                    Default: low",
+                "is_signed": "boolean. Sign or not created file. Optional.
+                    Default: false"
+            }
         Returns:
             Response: JSON response with success status and data or error status and error message.
-
         Raises:
             HTTPBadRequest: 400 HTTP error, if error.
-
         """
-
         pass
 
-    @UsersAPI.authorized
-    @RoleModel.role_model
+    # @UsersAPI.authorized
+    # @RoleModel.role_model
     # @UsersSQLAPI.authorized
     # @RoleModelSQL.role_model
-    async def delete_file(self, request: web.Request, *args, **kwargs) -> web.Response:
+    async def delete_file(self, request: web.Request,
+                          *args, **kwargs) -> web.Response:
         """Coroutine for deleting file.
 
         Args:
             request (Request): aiohttp request, contains filename.
-
         Returns:
-            Response: JSON response with success status and success message or error status and error message.
-
+            Response: JSON response with success status and success message
+            or error status and error message.
         Raises:
             HTTPBadRequest: 400 HTTP error, if error.
 
         """
-
         pass
 
     @UsersAPI.authorized
@@ -392,25 +388,24 @@ class Handler:
 
         pass
 
-    @UsersAPI.authorized
-    @RoleModel.role_model
+    # @UsersAPI.authorized
+    # @RoleModel.role_model
     # @UsersSQLAPI.authorized
     # @RoleModelSQL.role_model
-    async def change_file_dir(self, request: web.Request, *args, **kwargs) -> web.Response:
+    async def change_file_dir(self, request: web.Request,
+                              *args, **kwargs) -> web.Response:
         """Coroutine for changing working directory with files.
 
         Args:
-            request (Request): aiohttp request, contains JSON in body. JSON format:
+            request (Request): aiohttp request, contains JSON in body.
+            JSON format:
             {
                 "path": "string. Directory path. Required",
             }.
-
         Returns:
-            Response: JSON response with success status and success message or error status and error message.
-
+            Response: JSON response with success status and success message
+            or error status and error message.
         Raises:
             HTTPBadRequest: 400 HTTP error, if error.
-
         """
-
         pass
